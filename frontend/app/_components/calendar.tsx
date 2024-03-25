86261482a3f31a749ac7d20ea81799ab7a6d2cbd
dnjeh <ehdnj.001@gmail.com>
2024-03-25 20:14:24 +0900

    commit

--- frontend/app/_components/calendar.tsx
+++ frontend/app/_components/calendar.tsx
@@ -1,133 +1,92 @@
-"use client";
-
-import { ElementRef, RefObject, use, useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import CalendarDateWrap from "./calendar/calendarDateWrap";
+import { RefObject, useEffect } from "react";
 import styles from "./calendar.module.css";
-
-interface CalendarProps {
-  ymd: number[];
-  setymd?: ([]) => void;
-}
-
-interface DatesFace {
-  date: number;
-  mon: "pre" | "cur" | "nxt";
-}
-
+import Link from "next/link";
 interface MoveYmdProps {
   y?: number;
   m?: number;
   d?: number;
-  top?: string;
-  bot?: string;
   movingway: "pre" | "nxt" | "cur";
-  ref: RefObject<HTMLDivElement>;
 }
-
-const Month = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const Calendar = ({ ymd, setymd }: CalendarProps) => {
-  const currentDate = new Date();
-  const nowy = currentDate.getFullYear();
-  const nowm = currentDate.getMonth();
-  const nowd = currentDate.getDate();
-  const year = ymd[0];
-  const month = ymd[1];
-  const date = ymd[2];
-  const [isMoving, setIsMoving] = useState(false);
-
-  const wrapRef = useRef<ElementRef<"div">>(null);
-  const wrapWrapRef = useRef<ElementRef<"div">>(null);
-  const [nowRef, setNowRef] = useState(wrapRef);
-
-  const moveymd = ({ y, m, d, top, bot, movingway, ref }: MoveYmdProps) => {
-    if (!isMoving) {
-      let tt = y ? 0 : movingway == "cur" ? 0 : movingway == "nxt" ? 1 : -1;
-      let ty = 0,
-        tm = 0,
-        td = 0;
-        tm = tt;
-      if (tm + month < 0) {
-        ty=-1;
-      } else if (tm + month >= 12) {
-        ty=1;
-      }
-      let ry = y ? y : year + ty,
-        rm = m ? m : (month + tm + 12)%12,
-        rd = d ? d : date + td;
-      let prepct = top ? top :"0%",
-        nxtpct = bot ? bot : "-200%"; //타입스크립트 왜 썼지
-      if (setymd) {
-        if (movingway == "cur") {
-          setymd([ry, rm, rd]);
-        } else if (ref.current?.style.top != null) {
-          setIsMoving(true);
-          if (movingway == "pre") ref.current.style.top = prepct;
-          else if (movingway == "nxt") ref.current.style.top = nxtpct;
-          setTimeout(() => {
-            if (ref.current?.style.top != null) {
-              ref.current.style.top = "-100%";
-            }
-            setIsMoving(false);
-            setymd([ry, rm, rd]);
-          }, 500);
-        }
+interface CalendarDateProps {
+  year: number;
+  month: number;
+  date: number;
+}
+interface DatesFace {
+  date: number;
+  mon: "pre" | "cur" | "nxt";
+}
+const CalendarDate = ({
+  year,
+  month,
+  date,
+}: CalendarDateProps) => {
+  const Dates: DatesFace[][] = [];
+  const preMonthDate = new Date(year, month, 0);
+  const preMonthDay = preMonthDate.getDay();
+  const lastDay = new Date(year, month + 1, 0).getDate();
+  Dates.push([{ date: preMonthDate.getDate(), mon: "pre" }]);
+  
+  for (let i = 0; i < preMonthDay; i++) {
+    Dates[0].unshift({ date: preMonthDate.getDate() - i - 1, mon: "pre" });
+  }
+  for (let i = 0; i < 6; i++) {
+    for (; Dates[i].length < 7; ) {
+      const len = Dates[i].length - 1;
+      if (Dates[i][len].mon == "pre") {
+        Dates[i].push({ date: 1, mon: "cur" });
+      } else if (Dates[i][len].date == lastDay) {
+        Dates[i].push({ date: 1, mon: "nxt" });
+      } else {
+        Dates[i].push({
+          date: Dates[i][len].date + 1,
+          mon: Dates[i][len].mon,
+        });
       }
     }
-  };
-
-  const fadeymd = () => {
-    if (setymd && !isMoving) {
-      setIsMoving(true);
-      if (wrapWrapRef.current?.style.opacity != null) {
-        wrapWrapRef.current.style.opacity = "0";
+    if (i < 5) {
+      if (Dates[i][6].mon == "pre") {
+        Dates.push([{ date: 1, mon: "cur" }]);
+      } else if (Dates[i][6].mon == "cur" && Dates[i][6].date == lastDay) {
+        Dates.push([{ date: 1, mon: "nxt" }]);
+      } else {
+        Dates.push([{ date: Dates[i][6].date + 1, mon: Dates[i][6].mon }]);
       }
-      setTimeout(() => {
-        setymd([nowy, nowm, nowd]);
-        if (wrapWrapRef.current?.style.opacity != null) {
-          wrapWrapRef.current.style.opacity = "1";
-        }
-        setTimeout(() => {
-          setIsMoving(false);
-        }, 300);
-      }, 300);
     }
-  };
+  }
+
+  const getQueryString = (t:string, tt:number) => {
+    let tyear = t=="pre" && month == 0 ? year-1: t=="nxt" && month == 11? year +1 : year;
+    let d = t=="pre" ? -1: t=="nxt" ?1: 0;
+    let tmonth = (month+d+12)%12;
+    let tdate = tt;
+    return `/?year=${tyear}&month=${tmonth}&date=${tdate}`
+  }
 
   return (
-    <div className="h-full w-full p-8 flex flex-col items-start">
-      <div>
+    <>
+      {Dates.map((ai, i) => (
         <div
-          ref={wrapWrapRef}
-          className={cn(
-            "absolute w-[200%] left-0 h-full flex",
-            isMoving && "transition-all ease-ease duration-300"
-          )}
+          key={i}
+          className={styles.rows}
         >
-          <CalendarDateWrap
-            isMoving={isMoving}
-            wrapRef={wrapRef}
-            ymd={ymd}
-            moveymd={moveymd}
-            setymd={setymd}
-          />
+          {ai.map((aj, j) => (
+            <Link
+              key={j}
+              className={styles.cols}
+              href={getQueryString(aj.mon, aj.date)}
+            >
+              <div
+                className={styles.items + " " + (aj.mon !== "cur" ? styles.itemspp : "") + " " + (aj.mon==="cur" && aj.date === date ? styles.today :"")}
+              >
+                {aj.date}
+              </div>
+            </Link>
+          ))}
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
-export default Calendar;
+export default CalendarDate;
