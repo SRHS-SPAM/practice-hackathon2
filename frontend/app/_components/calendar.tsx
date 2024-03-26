f2d5e4072e4a7a7122b3a27a86bfdb9715e6eef3
dnjeh <ehdnj.001@gmail.com>
2024-03-26 17:42:14 +0900

    middle commit

--- frontend/app/_components/calendar.tsx
+++ frontend/app/_components/calendar.tsx
@@ -16,19 +16,28 @@ interface DatesFace {
   date: number;
   mon: "pre" | "cur" | "nxt";
 }
-const CalendarDate = ({
-  year,
-  month,
-  date,
-}: CalendarDateProps) => {
+const Months = ["January"
+,"February"
+,"March"
+,"April"
+,"May"
+,"June"
+,"July"
+,"August"
+,"September"
+,"October"
+,"November"
+,"December"
+]
+const CalendarDate = ({ year, month, date }: CalendarDateProps) => {
   const Dates: DatesFace[][] = [];
-  const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-  const dummy = [0, 0, 0, 0, 0, 0, 0,0 ,0 ,0 ,0 ,0, 0];
+  const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dummy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   const preMonthDate = new Date(year, month, 0);
   const preMonthDay = preMonthDate.getDay();
   const lastDay = new Date(year, month + 1, 0).getDate();
   Dates.push([{ date: preMonthDate.getDate(), mon: "pre" }]);
-  
+
   for (let i = 0; i < preMonthDay; i++) {
     Dates[0].unshift({ date: preMonthDate.getDate() - i - 1, mon: "pre" });
   }
@@ -57,42 +66,59 @@ const CalendarDate = ({
     }
   }
 
-  const getQueryString = (t:string, tt:number) => {
-    let tyear = t=="pre" && month == 0 ? year-1: t=="nxt" && month == 11? year +1 : year;
-    let d = t=="pre" ? -1: t=="nxt" ?1: 0;
-    let tmonth = (month+d+12)%12;
+  const getQueryString = (t: string, tt: number) => {
+    let tyear =
+      t == "pre" && month == 0
+        ? year - 1
+        : t == "nxt" && month == 11
+        ? year + 1
+        : year;
+    let d = t == "pre" ? -1 : t == "nxt" ? 1 : 0;
+    let tmonth = (month + d + 12) % 12;
     let tdate = tt;
-    return `/?year=${tyear}&month=${tmonth}&date=${tdate}`
-  }
+    return `/?year=${tyear}&month=${tmonth}&date=${tdate}`;
+  };
 
   return (
     <>
-    <div className={styles.wrap}>
-      <div className={styles.daywrap}>
-      {Days.map((ai, i) => (
-        <div key={i}>{ai}</div>
-      ))
-      }</div>
-      {Dates.map((ai, i) => (
-        <div
-          key={i}
-          className={styles.rows}
-        >
-          {ai.map((aj, j) => (
-            <Link
-              key={j}
-              className={styles.cols}
-              href={getQueryString(aj.mon, aj.date)}
-            >
-              <div
-                className={styles.items + " " + (aj.mon !== "cur" ? styles.itemspp : "") + " " + (aj.mon==="cur" && aj.date === date ? styles.today :"")}
-              >
-                {aj.date}
-              </div>
-            </Link>
+      <div className={styles.wrap}>
+        <div className={styles.deco}>
+          {dummy.map((ai, i)=>(
+            <div key={i}/>
           ))}
         </div>
-      ))}</div>
+        <div className={styles.yearwrap}>
+        {Months[month] + " " + year}
+        </div>
+        <div className={styles.daywrap}>
+          {Days.map((ai, i) => (
+            <div key={i}>{ai}</div>
+          ))}
+        </div>
+        {Dates.map((ai, i) => (
+          <div key={i} className={styles.rows}>
+            {ai.map((aj, j) => (
+              <Link
+                key={j}
+                className={styles.cols}
+                href={getQueryString(aj.mon, aj.date)}
+              >
+                <div
+                  className={
+                    styles.items +
+                    " " +
+                    (aj.mon !== "cur" ? styles.itemspp : "") +
+                    " " +
+                    (aj.mon === "cur" && aj.date === date ? styles.today : "")
+                  }
+                >
+                  {aj.date}
+                </div>
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
